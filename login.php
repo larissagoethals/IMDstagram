f83c97c70e841fa1cedfaae7abeb6d1955a2004d
yarondassonneville <yarondassonneville@outlook.com>
2016-03-07 20:16:12 +0100

    Made login & signup (1 & 2)

---
+++ login.php
@@ -0,0 +1,94 @@
+<?php
+    //login code
+    session_start();
+
+    //Include settings.php
+    include_once('settings.php');
+
+function canLogin( $p_username, $p_password ){
+    $conn = new mysqli('localhost', 'root', '', 'IMDstagram');
+
+    $sql = "select * from users
+            where username = '".$conn->real_escape_string($p_username)."'";
+
+    $result = $conn->query($sql);
+    if($result->num_rows == 1){
+        $user = $result->fetch_assoc();
+        $hash = $user['password'];
+
+        if(password_verify($p_password, $hash)) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else {
+        return false;
+    };
+}
+
+if( !empty( $_POST ) ){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if( canLogin( $username, $password ) ){
+        $_SESSION['loggedinFact'] = true;
+        $_SESSION['loggedin'] = "yes";
+        // redirect to index.php
+        header('location: timeline.php');
+    }
+    else{
+        // feedback
+        $error = "De gebruikersnaam en het wachtwoord zijn geen correcte combinatie. Gelieve opnieuw te proberen.";
+    }
+}
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>IMDstagram Login</title>
+    <link rel="stylesheet" href="style/reset.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="style/loginStyle.css">
+</head>
+<body>
+   <div class="outerBox">
+   
+    <div class="loginBox">
+        <div class="loginLogo">
+            
+        </div>
+        <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST">
+            <label>
+                <input type="text" name="username" placeholder="Gebruikersnaam" required>
+            </label>
+            <label>
+                <input type="password" name="password" placeholder="Wachtwoord" required>
+            </label>
+            <input type="submit" value="Aanmelden">
+        </form>
+        <div class="loginOr">
+            <div class="loginStripe"></div>
+            <div class="loginOrText">of</div>
+            <div class="loginStripe"></div>
+        </div>
+        <a href="#" class="viaFacebook">
+            <div class="logoFacebook"></div>
+            <div class="textFacebook">Aanmelden met Facebook</div>
+       </a>
+       <div class="errorText">
+           <?php if(isset($error)): ?>
+           <p><?php echo $error ?></p>
+           <?php else: ?>
+           <?php endif ?>
+       </div>
+       
+    </div>
+    
+    <div class="registreer">
+        <a href="signup.php" class="signup">Registreer</a>
+    </div>
+    </div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,81 @@
+<?php
+    //include settings.php
+    include_once('settings.php');
+
+    if(!empty($_POST)){
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $name = $_POST['name'];
+
+        $options = [
+            'cost' => 12
+        ];
+
+    //Password versleutelen
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT, $options);
+
+    //connectie
+    $conn = new mysqli($DB_LOCATION, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+
+    if($conn->connect_errno){
+        die("No database connection");
+    }
+
+    //query
+    $query = "INSERT INTO users(email, password, name, username) VALUES ('$email', '$password', '$name', '$username');" ;
+
+    //echo $query;
+    if($conn->query( $query )){
+        $success = "Welcome aboard!";
+    };
+}
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up - IMDstagram</title>
+    <link rel="stylesheet" href="style/reset.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="style/loginStyle.css">
+</head>
+<body>
+<div class="outerBox">
+
+    <div class="loginBox">
+        <div class="loginLogo">
+
+        </div>
+        <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST">
+            <label>
+                <input type="text" name="username" placeholder="Gebruikersnaam">
+            </label>
+            <label>
+                <input type="password" name="password" placeholder="Wachtwoord">
+            </label>
+            <label>
+                <input type="email" name="email" placeholder="E-mail">
+            </label>
+            <label>
+                <input type="text" name="name" placeholder="Naam">
+            </label>
+            <input type="submit" value="Registreer">
+        </form>
+        <a href="#" class="viaFacebook">
+            <div class="logoFacebook" style="margin-top:0px;"></div>
+            <div class="textFacebook" style="margin-top:0px;">Registreren met Facebook</div>
+        </a>
+        <div class="errorText">
+            <?php if(isset($error)): ?>
+                <p><?php echo $error ?></p>
+            <?php else: ?>
+            <?php endif ?>
+        </div>
+
+    </div>
+
+    <div class="registreer">
+        <a href="login.php" class="signup">Login</a>
+    </div>
+</div>
+</body>
+</html>
