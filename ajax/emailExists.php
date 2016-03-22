0aebcd128965b3bfd2f8fc4de25beb48681c5be6
Damon Baesen <kms26@MacBook-Pro-Damon-Baesen.local>
2016-03-22 15:33:23 +0100

    ajax upload (accountEdit)

---
+++ ajax/emailExists.php
@@ -0,0 +1,33 @@
+<?php
+// message ophalen $_POST
+
+// new activity --> save();
+
+//antwoorden in JSON
+
+include_once("../classes/User.class.php");
+$activity = new User();
+
+
+if(!empty($_POST['email'])) {
+    try {
+        $activity->Email = $_POST['email'];
+        if($activity->emailExists()) {
+            $response['status'] = 'exist';
+            $response['message'] = ":( Email not available!";
+        }
+        else
+        {
+            $response['status'] = 'notexist';
+            $response['message'] = ":) Email available!";
+        }
+    } catch (Exception $e) {
+        $feedback = $e->getMessage();
+        $response['status'] = 'error';
+        $response['message'] = $feedback;
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response); //{status: "error", message: ''}
+}
+?>
