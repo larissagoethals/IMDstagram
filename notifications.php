2cfc64ae62407b6135e4fc06a856d1a90c12b9d8
kms26 <kms26@MacBook-Pro-Damon-Baesen.local>
2016-05-12 09:12:52 +0200

    Fixed checklogin

--- notifications.php
+++ notifications.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+include_once('Includes/checklogin.php');
     include_once('classes/User.class.php');
     $user = new User();
     $user->Username = $_SESSION['username'];
